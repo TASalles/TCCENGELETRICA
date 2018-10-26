a34b774167be0b1d3d4ab4a9259fe505bfdb4f1f
TASalles <spinsalles@gmail.com>
2018-10-26 11:39:35 -0200

    Commit Inicial

---
+++ TCC_Eng_Eletrica/Assets/Guidao.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class Guidao : MonoBehaviour {
+
+    SerialPort sp;
+    //public float Valor;
+    private string Arduino;
+
+    public float Rotacao;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Rotacao = 0;
+        sp = new SerialPort("COM7", 9600);
+        sp.Open();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Rotate(0, Rotacao, 0);
+        Arduino = sp.ReadLine();
+        Rotacao = float.Parse(Arduino);
+		
+	}
+}
